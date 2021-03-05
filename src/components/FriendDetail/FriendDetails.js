0be7a2b7465f042f1d17b531dc579e153d898303
RianIslam <rianislamdr@gmail.com>
2021-03-06 01:50:17 +0600

    load dynamic data from friendId

--- src/components/FriendDetail/FriendDetails.js
+++ src/components/FriendDetail/FriendDetails.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router'
 
 const FriendDetails = () => {
     const {friendId} = useParams()
+    useEffect(()=>{
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data =>)
+    },[])
     return (
         <div>
             <h1>Friend Details {friendId}</h1>
