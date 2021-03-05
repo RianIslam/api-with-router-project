30e0822c78fabd403ece5bd50c1a4102cd48394b
RianIslam <rianislamdr@gmail.com>
2021-03-05 23:08:18 +0600

    add some style

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -3,9 +3,15 @@ import React from 'react'
 const Friend = (props) => {
     
     const {name,email} = props.friend
+    const friendStyle = {
+        border: '1px solid purple',
+        margin: '20px',
+        padding: '20px',
+        borderRadius: '20px'
+    }
 
     return (
-        <div>
+        <div style={friendStyle}>
             <h2>Name:{name}</h2>
             <p>email:{email}</p>
         </div>
